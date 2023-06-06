db4babf7a5d149d8ffebb6d52fc4f7ddbb02aa59
Carmina López <a00830265@itesm.mx>
2023-06-06 17:17:33 -0600

    Intento 2

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -40,7 +40,7 @@ export default function NotesPage() {
       <main className="flex h-full bg-white">
         <div className="h-full w-80 border-r bg-gray-50">
           <Link to="new" className="block p-4 text-xl text-blue-500">
-            + Add Note
+            + New Note
           </Link>
 
           <hr />
